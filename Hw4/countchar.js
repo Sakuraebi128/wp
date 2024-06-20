1da3793b41d3eaf62bbd89257685e2b3771c68ed
Sakuraebi128 <161835541+Sakuraebi128@users.noreply.github.com>
2024-06-20 14:17:37 +0800

    Create countchar.js

---
+++ Hw4/countchar.js
@@ -0,0 +1,14 @@
+function countChar(str) {
+  let charCount = {};
+
+  for (let char of str) {
+  if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+  return charCount;
+}
+
